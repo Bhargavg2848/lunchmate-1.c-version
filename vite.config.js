8e3ba86e822a5b8b4b66ebbb2530d63026be0845
Bhargavg2848 <100029318+Bhargavg2848@users.noreply.github.com>
2026-07-19 03:40:26 +0530

    Fix chaining and render-blocking CSS warnings

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin()
+  ],
 })
